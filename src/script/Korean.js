c2daefc0a107807890dd3ef2af23a99db2299d28
ob-ivan <ob-ivan@github.com>
2012-06-23 00:49:27 +0400

    Rename Korean.js to Capitallized Case

---
+++ src/script/Korean.js
@@ -0,0 +1,86 @@
+/**
+ * Превращает страницу с двумя видимыми формами в страничку с одной формой,
+ * переключаемой табами между двумя режимами.
+ *
+ * Использует jQuery.
+ *
+ * Ожидает такой раскладки страницы:
+ *  div.tabs
+ *      div.name
+ *      div.text
+ *  h2.name
+ *  div.name.container
+ *      input.korean
+ *  h2.text
+ *  div.text.container
+ *      textarea.korean
+**/
+var Korean = (function($)
+{
+    return function Korean (active_tab)
+    {
+        // var //
+        {
+            var tabs = $('.tabs'),
+                name_tab = tabs.find('.tab.name'),
+                text_tab = tabs.find('.tab.text'),
+                name_container = $('.name.container'),
+                text_container = $('.text.container'),
+                current_tab = null;
+        }
+        
+        // private //
+        {
+            var setName = function setName()
+            {
+                name_tab.addClass('active');
+                name_container.show();
+                text_tab.removeClass('active');
+                text_container.hide();
+                current_tab = 'name';
+            };
+            
+            var setText = function setText()
+            {
+                text_tab.addClass('active');
+                text_container.show();
+                name_tab.removeClass('active');
+                name_container.hide();
+                current_tab = 'text';
+            };
+        }
+        
+        // public //
+        {
+            this.getActiveInput = function()
+            {
+                return $('div.' + current_tab + '.container .korean');
+            };
+        }
+        
+        // constructor //
+        {
+            // switch from no JS version
+            $('h2').hide();
+            tabs.show();
+            
+            // enable active tab
+            if (active_tab)
+            {
+                if (active_tab == 'name')
+                {
+                    setName();
+                }
+                else if (active_tab == 'text')
+                {
+                    setText();
+                }
+            }
+            
+            // listen to events
+            name_tab.click(setName);
+            text_tab.click(setText);
+        }
+    };
+})(jQuery);
+
